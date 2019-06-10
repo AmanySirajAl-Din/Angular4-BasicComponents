17a3e76da7b9a1cd63eb597d384cd1fae7607645
Amany Sirajudin <amanysirajaldin@gmail.com>
2019-06-10 14:46:50 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
+ import { BrowserModule } from '@angular/platform-browser';
+
+// import NgModule decorator
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
+// has to import the ServerComponent before addin it
+import { ServerComponent } from './server/server.component';
+
+// this is the NgModule decorator
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule      
   ],
   providers: [],
   bootstrap: [AppComponent]
+    /* which component  this tells Angular which component 
+    should be aware of at the point of time 
+    that the whole app starts */
 })
 export class AppModule { }
+
+    
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,32 @@
+/* a componetent is just a typescript class 
+ so Angular is able to instantiat it 
+ and building objects aupon the class blue print */
+
+// to tell Angular that the class is a component
+// we will use a class decorator  
+
+import { Component } from '@angular/core';
+// this import to import the component decorator
+// import { files } from 'backage'
+// core is angular backage
+
+// decorator always defined by @ sing in the begining
+@Component({
+    // first we need to pass js objects
+    // to configure it and the class metadata
+    
+    selector: 'app-server',
+    /* (string) the html tag which will be used 
+        in other components templates
+        using app- to make sure it won't override
+        an original html tag'*/
+    
+    templateUrl: './server.component.html'
+    /* refer to an external template file
+        using a relative path */
+})
+ 
+
+ export class ServerComponent {
+     
+ }
