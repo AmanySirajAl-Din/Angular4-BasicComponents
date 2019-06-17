00d6fb1e30c7b5f2a6d15ce9ea66fa5b3905467d
Amany Sirajudin <amanysirajaldin@gmail.com>
2019-06-17 15:35:43 +0200

    Using inline style for servers component

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,12 +4,20 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-servers',
   //templateUrl: './servers.component.html',
   template:`
+    <p>
+      servers works!
+    </p>
     <app-server></app-server>
     <app-server></app-server>`, 
     // must have a template at all times
     // '...' for single line html code
     // `...` for multi lines html code
-  styleUrls: ['./servers.component.css']
+  //styleUrls: ['./servers.component.css']
+    styles:[`
+        p {
+            color: green;
+        }
+    `]
 })
 export class ServersComponent implements OnInit {
 
